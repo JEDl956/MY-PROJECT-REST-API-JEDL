389f671f9c58674a00c81cbac8e5a6c706a16e05
Jonathan E De Leon <jdele218@stu.southtexascollege.edu>
2026-04-07 23:03:15 -0500

    4/7/2026 started video

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
   schema: './src/data/schema.js',
   out: './src/data/migrations',
-  dialect: 'sqlite'
+  dialect: 'sqlite',
+  driver: 'd1-http',
 })
